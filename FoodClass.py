c2c42b60a97b528995d95c9873c5b99fb8e8e4b4
wylieholden <wylie_holden1@baylor.edu>
2023-02-17 16:59:43 -0600

    work

---
+++ FoodClass.py
@@ -0,0 +1,42 @@
+# Customer Class - that has the following attributes - customerid, name, address, email, phone, member_status (True or False).
+# The Customer class’s __init__ method should accept an argument for each attribute.
+# The Customer class should have accessor methods only for each attribute.
+
+# Transaction Class - that has the following attributes - date, item name,cost and customerid.
+# The Procedure class’s __init__ method should accept an argument for each attribute.
+# The Transaction class should have accessor methods only for each attribute.
+
+
+class Customer:
+    def __init__(self, customerid, name, address, email, phone, member_status):
+        self.__customerid = customerid
+        self.__name = name
+        self.__address = address
+        self.__email = email
+        self.__phone = phone
+        self.__member_status = member_status
+
+
+class Transaction:
+    def __init__(self, date, item_name, cost, customerid):
+        self.__date = date
+        self.__item_name = item_name
+        self.__cost = cost
+        self.__customerid = customerid
+
+    def trans_statement(self):
+        for key in dict:
+            if dict[key][3] == self.__customerid:
+                print(f"Order Item: {dict[key][1]} Price: {dict[key][2]}")
+
+    def calc_cost(self):
+        self.__cost = 0
+        for key in dict:
+            if dict[key][4] == self.__customerid:
+                self.__cost = +dict[key][3]
+
+    def member_disc(self):
+        print("")
+
+    def get_cost(self):
+        return self.__cost
--- FoodProgram.py
+++ FoodProgram.py
@@ -1,11 +1,26 @@
 import FoodClass as fc
-# this dictionary represents transactions. The key of the dictionary is the 
-transaction identifier.
-# The Value of the dictionary is a list. The elements in each list are -
-# ['Date', 'Name of item', 'Cost', 'customerid' ]
-dict = {'trans1':['2/15/2023','The Lone Patty',17,569],
-                'trans2':['2/15/2023','The Octobreakfast',18,569],
-                'trans3':['2/15/2023','The Octoveg',16,570],
-                'trans4':['2/15/2023','The Octoburger',20,570],
+
+dict = {
+    "trans1": ["2/15/2023", "The Lone Patty", 17, 569],
+    "trans2": ["2/15/2023", "The Octobreakfast", 18, 569],
+    "trans3": ["2/15/2023", "The Octoveg", 16, 570],
+    "trans4": ["2/15/2023", "The Octoburger", 20, 570],
 }
 order_total = 0
+
+
+# customer obejct 1
+customerid = "570"
+name = "Danni Sellyar"
+address = "97 Mitchell Way Hewitt Texas 76712"
+email = "dsellyarft@gmpg.org"
+phone = "254-555-9362"
+member_status = "False"
+customer = fc.Customer(customerid, name, address, email, phone, member_status)
+
+# transaction 1
+date = "2/15/2023"
+item_name = "The Lone Patty"
+cost = "16"
+cusomterid = 569
+transaction1 = fc.Transaction(date, item_name, cost, customerid)
